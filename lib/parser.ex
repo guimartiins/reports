68a3b5264be647c1b8717b3d70ca1a26c0454e3c
Guilherme Martins <academicgmag@gmail.com>
2023-11-22 15:04:44 -0300

    add all files

---
+++ lib/parser.ex
@@ -0,0 +1,15 @@
+defmodule ReportsGenerator.Parser do
+  def parse_file(filename) do
+    "reports/#{filename}"
+    |> File.stream!()
+    |> Stream.map(fn line -> parse_line(line) end)
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    # & é outra forma de escrever uma função anonima
+    |> List.update_at(2, &String.to_integer/1)
+  end
+end
